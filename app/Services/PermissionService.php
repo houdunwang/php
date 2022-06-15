c3696fc0ec25c5abeeab180f761da19b0d93b10c
houdunren <2300071698@qq.com>
2022-06-16 02:51:16 +0800

    删除模块配置中不存在的权限

--- app/Services/PermissionService.php
+++ app/Services/PermissionService.php
@@ -10,6 +10,8 @@ class PermissionService
 {
     protected $site;
 
+    protected $names = [];
+
     // 初始化站点模块权限
     public function syncAllModulePermissions(Site $site)
     {
@@ -17,38 +19,26 @@ class PermissionService
             $file = base_path("addons/{$module['name']}/Config/permissions.php");
             if (!is_file($file)) return;
 
-            $this->removeNotExistPermissions($site, $file, $module);
-
             collect(include $file)->each(function ($permission) use ($site, $module) {
                 $this->syncPermissions($site, $module, $permission);
             });
         });
-    }
 
-    // 删除模块配置中不存在的站点权限
-    protected function removeNotExistPermissions($site, $file, $module)
-    {
-        $names = collect(include $file)->reduce(function ($names, $permission) use ($module) {
-            $moduleNames = collect($permission['items'])->reduce(function ($names, $item) use ($module) {
-                return $names->push($module['name'] . '_' . $item['name']);
-            }, collect([]));
-
-            return [...$names, ...$moduleNames->toArray()];
-        }, []);
-
-        ModelsPermission::where('site_id', $site->id)->whereNotIn('name', $names->toArray())->delete();
+        // 删除模块配置中不存在的站点权限
+        ModelsPermission::where('site_id', $site->id)->whereNotIn('name', $this->names)->delete();
     }
 
     // 同步站点权限
     protected function syncPermissions(Site $site, $module, $permission): void
     {
         collect($permission['items'])->each(function ($item) use ($site, $module) {
+            $name = $module->name . "_" . $item['name'];
             $data = [
                 'site_id' => $site->id,
                 'module_id' => $module->id,
-                'name' => $module->name . "_" . $item['name'],
+                'name' => $name,
             ] + $item;
-
+            $this->names[] = $name;
             ModelsPermission::updateOrCreate($data);
         });
     }
