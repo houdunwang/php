3a6205784c089e41ad3ae3b52d78c58697b722a5
houdunren <2300071698@qq.com>
2022-07-09 04:28:58 +0800

    fix:模块域名主页访问

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Models\Site;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function (Request $request) {
+    $site = Site::where('url', $request->fullUrl())->firstOrFail();
+    $moduleHtml = public_path("addons/{$site->module->name}/dist/index.html");
+
+    return file_get_contents(is_file($moduleHtml) ? $moduleHtml : public_path('core/index.html'));
+});
 
 Route::fallback(function ($path = 'core') {
     $pathinfo = explode('/', $path);
