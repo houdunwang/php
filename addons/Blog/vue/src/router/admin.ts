129fc7a660d4ec63d845eee2445f4b3250e1e136
houdunren <2300071698@qq.com>
2022-07-08 20:44:11 +0800

    修复文档图片

--- addons/Blog/vue/src/router/admin.ts
+++ addons/Blog/vue/src/router/admin.ts
@@ -2,15 +2,15 @@ import { DashboardOne } from '@icon-park/vue-next'
 import { RouteRecordRaw } from 'vue-router'
 export default {
   name: 'admin',
-  path: '/admin',
+  path: '/Blog/admin',
   component: () => import('@@/layouts/admin.vue'),
   meta: { order: 1, auth: true, menu: { title: 'Dashboard', icon: DashboardOne } },
   children: [
     {
       name: 'admin.index',
       path: '',
-      component: () => import('@/views/admin/index.vue'),
-      meta: { menu: { title: '工作台' } },
+      component: () => import('@/views/admin/article/index.vue'),
+      meta: { menu: { title: '文章列表' } },
     },
   ],
 } as RouteRecordRaw
---
+++ addons/Blog/vue/src/views/admin/article/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>aa</template>
+
+<style lang="scss"></style>
