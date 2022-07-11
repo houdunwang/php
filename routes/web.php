211cbc8153645fa43a57e0ad720ef1756ae7a772
houdunren <2300071698@qq.com>
2022-07-12 03:01:29 +0800

    fix:安装失败的问题

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (Request $request) {
     $site = Site::where('url', $request->host())->first();
 
-    if (!$site || !$site->module) return view('404', ['message' => '域名没有绑定站点，或站点没有设置默认模块']);
+    if (!$site || !$site->module) {
+        return view('404', ['message' => '域名没有绑定站点，或站点没有设置默认模块']);
+    }
 
     $moduleHtml = public_path("addons/{$site->module->name}/dist/index.html");
     return file_get_contents(is_file($moduleHtml) ? $moduleHtml : public_path('core/index.html'));
@@ -15,9 +17,10 @@ Route::get('/', function (Request $request) {
 
 Route::fallback(function ($path = 'core') {
     $pathinfo = explode('/', $path);
-    $dirname = $pathinfo[0] == 'core' ? 'core' : "addons/$pathinfo[0]/dist";
+    $dirname = preg_match('/core|install/i', $pathinfo[0]) ? 'core' : "addons/$pathinfo[0]/dist";
 
     $template = public_path($dirname . '/index.html');
     if (!is_file($template)) return view('404');
+
     return file_get_contents($template);
 });
