fa0fd9021e0da54ce09bf89bbbe12f122bc8f56e
houdunren <2300071698@qq.com>
2022-07-12 03:44:20 +0800

    fix:修复程序安装

--- app/Http/Controllers/InstallController.php
+++ app/Http/Controllers/InstallController.php
@@ -15,17 +15,18 @@ class InstallController extends Controller
     }
     public function testLink(Request $request)
     {
-        $config =$request->input('database')+ [
+        $config = $request->input('database') + [
             'host' =>  '127.0.0.1',
             'port' =>  '3306',
             'database' => 'php',
             'username' =>  'root',
             'password' => 'admin88a8'
         ];
+
         try {
-            $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $config['host'], $config['database'], 'utf8');
+            $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $config['host'], $config['database'] ?? '@@', 'utf8');
             new \PDO($dsn, $config['username'], $config['password']);
-            file_put_contents(base_path('config.php'), '<?php return '.var_export($request->input(), true).';');
+            file_put_contents(base_path('config.php'), '<?php return ' . var_export($request->input(), true) . ';');
         } catch (\PDOException $e) {
             abort(403, '数据库连接失败');
         }
